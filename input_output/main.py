16e1c1eb51c2baeeef944713c0794f544ad7d538
polokswe <poloknn@gmail.com>
2026-05-25 11:11:02 +0600

    fix input_output as normal folder

---
+++ input_output/main.py
@@ -0,0 +1,12 @@
+import os
+import cv2
+
+image_path = os.path.join ('sigma.png')
+image = cv2.imread(image_path)
+
+cv2.imwrite(os.path.join('sigma_clone.png'),image)
+
+cv2.imshow("Clone", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
